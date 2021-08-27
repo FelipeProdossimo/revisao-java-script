79046d77006dfc2749a741ea13950db0f233d3fa
FELIPE PRODOSSIMO <felipe.prodossimo@senacsp.edu.br>
2021-08-26 22:29:52 -0300

    Site de anime quase pronto, faltando links e modificar pontuação por estrelas!

--- 03-filmes/js/app.js
+++ 03-filmes/js/app.js
@@ -1,43 +1,82 @@
-function CarregarAnimes() {
-    let objetoAnimes = [
-        {
-            foto: "black-clover.jpg",
-            nome: "Black Clover"
-        },
-        {
-            foto: "boku-no-hero.jpg",
-            nome: "Boku No Hero Academy"
-        },
-        {
-            foto: "nanatsu.jpg",
-            nome: "Nanatsu No Taizai"
-        },
-        {
-            foto: "tokyo.jpg",
-            nome: "Tokyo Ghoul"
-        },
-        {
-            foto: "naruto-ship.jpg",
-            nome: "Naruto Shippuuden"
-        },
-        {
-            foto: "one-piece.jpg",
-            nome: "One Piece"
+function ConsultaAPI() {
+    const animeAPI = "https://api.jikan.moe/v3/season/2021/spring";
+    let objetoAnimes = [];
+    $.ajax({
+        url: animeAPI,
+        async: false,
+        success: function(dados) {
+            objetoAnimes = dados.anime;
+
         }
-    ]
-    let listaAnimes = document.querySelector(".lista-animes");
-    let cartao = "";
-    for (let i = 0; i < objetoAnimes.length; i++) {
-        cartao += "<button>";
-        cartao += "<div class='cartao'>";
-        cartao += "<img src='img/" + objetoAnimes[i].foto + "'>";
-        cartao += objetoAnimes[i].nome;
-        cartao += "</div>"
-        cartao += "</button>";
-    }
-    listaAnimes.innerHTML = cartao;
+    });
+
+    return objetoAnimes;
 }
 
-window.onload = function () {
+ConsultaAPI();
+
+/*
+
+ANIMES FIXOS - PREPARADOS
+
+let objetoAnimes = [{
+        foto: "black-clover.jpg",
+        nome: "Black Clover"
+    },
+    {
+        foto: "boku-no-hero.jpg",
+        nome: "Boku No Hero Academy"
+    },
+    {
+        foto: "nanatsu.jpg",
+        nome: "Nanatsu No Taizai"
+    },
+    {
+        foto: "tokyo.jpg",
+        nome: "Tokyo Ghoul"
+    },
+    {
+        foto: "naruto-ship.jpg",
+        nome: "Naruto Shippuuden"
+    },
+    {
+        foto: "one-piece.jpg",
+        nome: "One Piece"
+    }
+]
+*/
+
+function MontarCartao(foto, nome, score, url) {
+    let cartao = "";
+    cartao += "<button class='botao' href='" + url + "'>";
+    cartao += "<div class='cartao'>";
+    cartao += "<img class='botao-img'  src='" + foto + "'>";
+    cartao += "<h4 class='title-card'>" + nome + "</h4>";
+    cartao += "<h4>" + score + "</h4>"
+
+    cartao += "</div>"
+    cartao += "</button>";
+    return cartao;
+}
+
+function CarregarAnimes() {
+
+    let listaAnimes = document.querySelector(".lista-animes");
+    let conjuntoDeCartoes = "";
+    let todosAnimes = ConsultaAPI();
+
+    todosAnimes.forEach(animeAtivo => {
+        conjuntoDeCartoes += MontarCartao(animeAtivo.image_url, animeAtivo.title, animeAtivo.score, animeAtivo.url);
+    });
+    listaAnimes.innerHTML = conjuntoDeCartoes;
+}
+
+//Carregar por botão
+//document.querySelector("#btAnimes").onclick = function() {
+//CarregarAnimes();
+//}
+
+//Carregar só por abrir a página
+window.onload = function() {
     CarregarAnimes();
 }
