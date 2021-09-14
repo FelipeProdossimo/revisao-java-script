2bc74d517acdf59fe6e85dd21e738b7db5d0436e
FELIPE PRODOSSIMO <felipe.prodossimo@senacsp.edu.br>
2021-09-14 19:39:16 -0300

    Super Trunfo Atualizado, da aula anterior!

--- 05-super-trunfo/js/app.js
+++ 05-super-trunfo/js/app.js
@@ -1,10 +1,11 @@
+// Criado os objetos CARTA com seus atributos.
 const naruto = {
     nome: "Naruto",
     imagem: "img/naruto.png",
-    atributo: {
-        ataque: 8,
-        defesa: 7,
-        magia: 3
+    atributos: {
+        ataque: 7,
+        defesa: 6,
+        magia: 2
     }
 }
 
@@ -16,39 +17,114 @@ const batman = {
         defesa: 8,
         magia: 0
     }
-};
+}
 
-const cartas = [naruto , batman];
+const darthVader = {
+    nome: "Darth Vader",
+    imagem: "img/darthvader.jpg",
+    atributos: {
+        ataque: 8,
+        defesa: 5,
+        magia: 3
+    }
+}
+
+const harry = {
+    nome: "Harry Potter",
+    imagem: "img/harry.jpg",
+    atributos: {
+        ataque: 6,
+        defesa: 4,
+        magia: 3
+    }
+}
+
+const cartas = [naruto, batman, darthVader, harry];
 let cartaJogador, cartaMaquina;
 
+
+// Criado função de Sortear as cartas
 function SortearCarta(){
-    const numeroDeCartas = 2;
+    const numeroDeCartas = 4;
     let numeroCartaJogador = parseInt(Math.random()*numeroDeCartas);
     let numeroCartaMaquina = parseInt(Math.random()*numeroDeCartas);
-    while(numeroCartaJogador == numeroCartaMaquina){
+    
+    // Laço para não repetir as cartas entres os competidores
+    while (numeroCartaJogador == numeroCartaMaquina){
         numeroCartaJogador = parseInt(Math.random()*numeroDeCartas);
     }
+    
     cartaJogador = cartas[numeroCartaJogador];
     cartaMaquina = cartas[numeroCartaMaquina];
-
-    //para mostrar no console antes
-    //console.log(cartaJogador, cartaMaquina)
-    ExibirCartaJogador();
+    
+    
 }
 
+// Exibir a carta do Jogador e carta Maquina
 function ExibirCartaJogador(){
-    console.log(cartaJogador);
     let divCartaJogador = document.querySelector("#carta-jogador");
-    //Forma antiga do JS
-    //divCartaJogador.style.backgroundImage = 'url(' + cartaJogador.imagem + ')';
-    //Forma Nova
     divCartaJogador.style.backgroundImage = `url(${cartaJogador.imagem})`;
-    document.querySelector(".nome-persongem-jogador").innerText = cartaJogador.nome;
-    cartaJogador.nome;
+    document.querySelector(".nome-personagem-jogador").innerText = cartaJogador.nome;
 
+    //pegar e listar atributos Jogador
+    let listaDeAtributosDaCarta = document.querySelector(".atributos-jogador");
+    listaDeAtributosDaCarta.innerHTML = PegarAtributos(cartaJogador.atributos);
 }
 
+function ExibirCartaMaquina(){
+    let divCartaMaquina = document.querySelector("#carta-maquina");
+    divCartaMaquina.style.backgroundImage = `url(${cartaMaquina.imagem})`;
+    document.querySelector(".nome-personagem-maquina").innerText = cartaMaquina.nome;
+
+    //pegar e listar atributos Máquina
+    let listaDeAtributosDaCarta = document.querySelector(".atributos-maquina");
+    listaDeAtributosDaCarta.innerHTML = PegarAtributosMaquina(cartaMaquina.atributos);
+}
+
+
+function PegarAtributosMaquina(){
+    let listaDeAtributos = "";
+    for(let atributo in atributosDaCarta){
+        listaDeAtributos += `<li>$(atributo) : $(atributosDaCarta)[atributo]</li>`;
+    }
+    
+    return listaDeAtributos;
+}
+
+
+function PegarAtributos(atributosDaCarta){
+    let listaDeAtributos = "", botaoRadio="";
+    for(let atributo in atributosDaCarta){
+        botaoRadio = ` <input type="radio" name="atributo" value="${atributo}">`;
+        listaDeAtributos += `<li> ${atributo} : ${atributosDaCarta[atributo]} ${botaoRadio}</li>`;
+    }
+    return listaDeAtributos;
+}
+
+function Jogar(){
+    
+}
+
+const btnJogar = document.querySelector("#btnJogar");
+btnJogar.onclick = () => {   
+    ExibirCartaMaquina();
+  
+    // O código desejado é apenas isto:
+    //document.getElementById("loading").style.display = "none";
+    //document.getElementById("conteudo").style.display = "inline";
+
+}//, 4000;
+;
+
+// Inserindo evento no botão SORTEAR_CARTA.
 const btnSortear = document.querySelector("#btnSortear");
 btnSortear.onclick = () => {
     SortearCarta();
-}
+    ExibirCartaJogador();
+    document.querySelector("#btnJogar").disabled = false;
+};
+
+
+
+
+
